7abc90e1df7537c91fefacfff909731f1fc3737d
GaBizes <gabrieladossantosferreira97@gmail.com>
2025-05-19 20:48:42 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -23,5 +23,23 @@ int main(){
          i++;
      } while (i < 5);
 
+     printf("Implementação de Movimentação do Cavalo.\n");
+     // Movimento do Cavalo: duas casas para baixo
+     int j = 0;
+     int movimentoparaBaixo = 2;
+     int movimentoparaCima = 1;
+
+     
+     for (i = 0; i < movimentoparaBaixo; i++){
+          printf("Baixo.\n");
+
+       j = 0;
+       while (j < movimentoparaCima){
+           printf("Esquerda. \n");
+           j++;
+        }
+      }
+
      return 0;
+
 }
